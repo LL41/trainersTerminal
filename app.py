6779c28e2d49dc266b06663b8be328feb6858ff6
LL41 <logueluke41@gmail.com>
2024-11-10 18:57:28 -0800

    Added several files.

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ genai.configure(api_key=os.getenv("GENAI_KEY"))
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Redirect URI for OAuth
-redirect_uri = 'https://trainer-terminal-bee0e2ad6ae5.herokuapp.com'
+#redirect_uri = 'https://trainer-terminal-bee0e2ad6ae5.herokuapp.com'
+redirect_uri = 'http://127.0.0.1:5000/auth/callback'
 
 def trips_around_world(distance):
   earth_circumference = 24901  # miles
